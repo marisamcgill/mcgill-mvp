a070620f18daf016ca471508ca9fff7a5ea9af8f
Marisa-McGill <marisamcgill@hotmail.co.uk>
2023-11-07 11:18:05 +0000

    set up react routes for home and results

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+​
+export default function Home() {
+  return <div>Home</div>;
+}
---
+++ client/src/pages/Results.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+​
+export default function Results() {
+  return <div>Results</div>;
+}
